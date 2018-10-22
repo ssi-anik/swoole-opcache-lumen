fcc56617718756279acc659dc83c40cc4f8946f3
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2018-10-23 00:40:09 +0600

    Nginx & PHP-fpm (No opcache, swoole)

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return [
+        'framework' => 'Lumen',
+        'version' => $router->app->version(),
+    ];
+});
+
+$router->get('users', function () {
+    return [
+        'users' => \App\User::take(50)->offset(1000)->get(),
+    ];
+});
